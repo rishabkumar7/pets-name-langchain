dc7c25380c1ef5bbe04d29501cfcb10ad5105d1d
Rishab Kumar <rishabkumar7@gmail.com>
2023-07-16 14:18:45 -0400

    Suggests 5 names now

--- langchain_helper.py
+++ langchain_helper.py
@@ -15,7 +15,7 @@ def generate_pet_name(animal_type, pet_color):
 
     prompt_template_name = PromptTemplate(
         input_variables = ['animal_type','pet_color'],
-        template = "I have a {animal_type} pet and I want a cool name for it, it is {pet_color} in color. Suggest me a cool name for my pet."
+        template = "I have a {animal_type} pet and I want a cool name for it, it is {pet_color} in color. Suggest me five cool names for my pet."
     )
 
     name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="pet_name")
